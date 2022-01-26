3c3e51c00d608030eaabf18da8e88d6621dadda7
sussywebdesign <ytans19@gmail.com>
2022-01-26 10:57:16 -0500

    shop list made

---
+++ shop.js
@@ -0,0 +1,36 @@
+
+
+
+const items = [
+    {
+        name: "cooki",
+        price: "three dollar",
+        image:"https://i.redd.it/rgwsmr12zks61.jpg",
+
+    },
+    {
+        name:"sosig",
+        price:"2 pepsi caps",
+        image:"https://www.praseks.com/wp-content/uploads/2020/05/sausage-pork-beef-2.jpg",
+    },
+    {
+        name:"langosta larry",
+        price:"dust and lint",
+        image:"https://static.wikia.nocookie.net/unanything/images/c/cc/Larry.png/revision/latest?cb=20160125150432",
+    },
+    {
+        name:"YO MAMA",
+        price:"SO FAT",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGz8ZlY85X5_fkawE5ZovJqptMUFPxXeNnQA&usqp=CAU",
+    },
+    {
+        name:"fnaf at freddy",
+        price:"$8.99",
+        image:"http://i.imgur.com/JTFraHK.png",
+    },
+    {
+        name:"warrior",
+        price:"$89.99",
+        image:"https://c.tenor.com/XpkRp4Qb9OEAAAAM/epic-heavy.gif",
+    },
+];
